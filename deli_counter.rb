5a7f180958c0b6d5b016d0373583a2e3a22b88dd
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-08-02 04:32:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     print "The line is currently: "
@@ -16,7 +16,7 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
